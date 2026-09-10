7eafb85a12ecd77fa56875476118d21ef0b739c9
lesifikon <rob.val.2008@mail.ru>
2026-09-10 21:50:04 +0500

    написал ячейки в которых быдет находиться информация
    написал ячейки которые будут держать в себе 3 сочтояния

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 use rand::prelude::*;
 
 fn main() {
-
-    fn random(bomb: &mut u8) -> u8 {
-        if rand::rng().random_range(0..4) == 0 {
-            *bomb -= 1;
-            1
-        } else {
-            0
-        }
-    }
-
     let mut bomb_map = [0; 36];
-    let mut bomb = 6;
+    let mut bomb: u8 = 6;
 
     'onecycle: loop {
-
         for i in 0..bomb_map.len() {
             if bomb_map[i] == 1 {
                 continue;
@@ -27,11 +16,45 @@ fn main() {
                 }
             }
         }
-
     }
 
+    fn random(bomb: &mut u8) -> u8 {
+        if rand::rng().random_range(0..4) == 0 {
+            *bomb -= 1;
+            1
+        } else {
+            0
+        }
+    }
 
     println!("{:?}", bomb_map);
+
+
+    #[derive(Debug)]
+    enum Inside {
+        Bomb,
+        Number(u8),
+        Nule,
+    }
+
+    #[derive(Debug)]
+    struct Cell {
+        open: bool,
+        inside: Inside,
+    }
+
+    let mut cell_map = Vec::new();
+
+    for i in 0..bomb_map.len() {
+        let new_cell = Cell {
+            open: false,
+            inside: if bomb_map[i] == 1 {Inside::Bomb} else {Inside::Nule},
+        };
+        cell_map.push(new_cell);
+    }
+
+    println!("{:#?}", cell_map);
+
     // let mut bomb = 20; 
 
     let rows = 6;
