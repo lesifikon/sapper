b1ca0a162a8cb9c059cc61acc79b06b749e65b95
lesifikon <rob.val.2008@mail.ru>
2026-09-10 14:58:17 +0500

    ничего не смог

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,44 @@ use rand::prelude::*;
 
 fn main() {
 
+    // fn minelayer(bomb: i8) -> char {
+    //     let chance = rand::rng().random_range(1..=4);
+    //     println!("{}", bomb);
+    //     if chance == 4 && bomb > 0{
+    //         'B'
+    //     } else {
+    //         ' '
+    //     }
+    //
+    // }
+
     fn minelayer() -> char {
         let chance = rand::rng().random_range(1..=4);
+        // println!("{}", bomb);
         if chance == 4 {
             'B'
         } else {
             ' '
         }
-    //     'B'
 
     }
-    sad
-    // let mut bomb = 30;
+
+    // let mut bomb = 20;
 
     let rows = 6;
     let cols = 6;
 
     let mut map = vec![vec![' '; cols]; rows];
 
+    
     for i in 0..rows {
         for j in 0..cols{
             map[i][j] = minelayer();
+            // bomb -= 1
         }
     }
 
-    
+    // println!("{}", bomb);
 
 
     let size = map.len();
