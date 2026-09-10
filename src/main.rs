872498ec33493a45957642bf862c4c413351dd93
lesifikon <rob.val.2008@mail.ru>
2026-09-10 20:34:54 +0500

    генирация карты_бомб
    теперь при воде колическо клеток и количество бомб генируеться
    случайнная карта бомб

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 use rand::prelude::*;
 
-
-
 fn main() {
 
-    // fn minelayer(bomb: i8) -> char {
-    //     let chance = rand::rng().random_range(1..=4);
-    //     println!("{}", bomb);
-    //     if chance == 4 && bomb > 0{
-    //         'B'
-    //     } else {
-    //         ' '
-    //     }
-    //
-    // }
-
-    fn minelayer() -> char {
-        let chance = rand::rng().random_range(1..=4);
-        // println!("{}", bomb);
-        if chance == 4 {
-            'B'
+    fn random(bomb: &mut u8) -> u8 {
+        if rand::rng().random_range(0..4) == 0 {
+            *bomb -= 1;
+            1
         } else {
-            ' '
+            0
+        }
+    }
+
+    let mut bomb_map = [0; 36];
+    let mut bomb = 6;
+
+    'onecycle: loop {
+
+        for i in 0..bomb_map.len() {
+            if bomb_map[i] == 1 {
+                continue;
+            } else {
+                if bomb > 0 {
+                    bomb_map[i] = random(&mut bomb);
+                } else {
+                    break 'onecycle;
+                }
+            }
         }
 
     }
 
-    // let mut bomb = 20;
+
+    println!("{:?}", bomb_map);
+    // let mut bomb = 20; 
 
     let rows = 6;
     let cols = 6;
@@ -36,7 +42,7 @@ fn main() {
     
     for i in 0..rows {
         for j in 0..cols{
-            map[i][j] = minelayer();
+            map[i][j] = '#';
             // bomb -= 1
         }
     }
