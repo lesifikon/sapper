6381467e80b7055450da3146973b95272b3a2cc6
lesifikon <rob.val.2008@mail.ru>
2026-09-12 21:02:41 +0500

    убрал проблемму с цифрами
    код ужастно полохо написано но я раб что хотябы работает как надо

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use rand::prelude::*;
 
 fn main() {
-    let mut bomb_map = [0u8; 81];
-    let mut bomb: u8 = 8;
 
+    let rows = 9;
+    let cols = 9;
+
+    let mut map = [0u8; 81];
+    let mut mine: u8 = 8;
 
     'onecycle: loop {
-        for i in 0..bomb_map.len() {
-            if bomb_map[i] == 9 {
+        for i in 0..map.len() {
+            if map[i] == 9 {
                 continue;
             } else {
-                if bomb > 0 {
-                    bomb_map[i] = random(&mut bomb);
+                if mine > 0 {
+                    map[i] = random(&mut mine);
                 } else {
                     break 'onecycle;
                 }
@@ -20,52 +23,68 @@ fn main() {
     }
     // настроил lazygit чтобы не просил каждый раз
 
-    fn random(bomb: &mut u8) -> u8 {
+    fn random(mine: &mut u8) -> u8 {
         if rand::rng().random_range(0..20) == 0 {
-            *bomb -= 1;
+            *mine -= 1;
             9
         } else {
             0
         }
     }
 
-    // println!("{:?}", bomb_map);
+    // println!("{:?}", map);
 
-    for i in 0..bomb_map.len() {
-        let j = i as i16;
-        match bomb_map[i] {
-            0 => (),
-            9 => { numbers(j, &mut bomb_map); },
-            _ => (),
-        };
+    for i in 0..map.len() {
+        if map[i] == 9 {
+            numbers((i as i8).into(),rows, &mut map);
+        }
     }
 
-    fn numbers(place: i16, map: &mut [u8; 81]) {
-        for i in ((place - 10)..=(place - 8)).chain((place - 1)..=(place + 1)).chain((place + 8)..=(place + 10)) {
-            if i >= 0 && i <= 80 && map[i as usize] != 9{
+    fn numbers(place: i16, row: i16, map: &mut [u8]) {
+        let mut left = vec![0];
+        let mut right = vec![row - 1];
+
+
+        let x = (map.len() as i16) / row;
+        let mut bad_numbers = row;
+        for j in 2..=row {
+            left.push(bad_numbers);
+            right.push(bad_numbers + row - 1);
+            bad_numbers += row;
+        }
+
+        for i in ((place - (row + 1))..=(place - (row - 1))).chain((place - 1)..=(place + 1)).chain((place + (row - 1))..=(place + (row + 1))) {
+            if i >= 0 && i <= (map.len() as i16) - 1 && map[i as usize] != 9 {
                 map[i as usize] += 1;
             }
         }
-    }
 
-    fn ap_bomb(palce: u8, map: &mut [u8; 81]) {
-        for i in 1..=3 {
-            let new_palce = (palce - (11 - i)) as usize;
-            if map[new_palce] == 9 {
-                continue;
-            } else {
-                map[new_palce] = 1;
+        if left.contains(&place) {
+            let y = [place - row - 1, place - 1, place + row - 1];
+            for i in 0..y.len() {
+                let z = y[i];
+                if z > 0 && map[z as usize] != 9 {
+                    map[z as usize] -= 1;
+                }
             }
-
+        } else if right.contains(&place) {
+            let y = [place - row + 1, place + 1, place + row + 1];
+            for i in 0..y.len() {
+                let z = y[i];
+                if z > 0 && map[z as usize] != 9 {
+                    map[z as usize] -= 1;
+                }
+            }
         }
     }
 
-    // println!("{:?}", bomb_map);
+
+    // println!("{:?}", map);
 
 
     #[derive(Debug)]
     enum Inside {
-        Bomb,
+        Mine,
         Flag,
         Number(u8),
         Nule,
@@ -79,36 +98,34 @@ fn main() {
 
     let mut cell_map = Vec::new();
 
-    for i in 0..bomb_map.len() {
+    for i in 0..map.len() {
         let new_cell = Cell {
             open: false,
-            inside: if bomb_map[i] == 1 {Inside::Bomb} else {Inside::Nule},
+            inside: if map[i] == 1 {Inside::Mine} else {Inside::Nule},
         };
         cell_map.push(new_cell);
     }
 
     // println!("{:#?}", cell_map);
 
-    let rows = 9;
-    let cols = 9;
 
-    let mut map = vec![vec![ 0 ; cols]; rows];
+    let mut screen = vec![vec![ 0 ; cols]; rows as usize];
 
     let mut check = 0;
 
     for i in 0..rows {
         for j in 0..cols{
-            map[i][j] = bomb_map[check];
+            screen[i as usize][j] = map[check];
             check += 1;
         }
     }
 
-    // println!("{}", bomb);
+    // println!("{}", mine);
 
 
-    let size = map.len();
+    let size = screen.len();
 
-    for (i, row) in map.iter().enumerate() {
+    for (i, row) in screen.iter().enumerate() {
         for (j, cell) in row.iter().enumerate() {
             print!(" {} ", cell);
             if j < size - 1 {
