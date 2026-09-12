7e564398c77a1e18a4756148bd8d0eb0abc35799
lesifikon <rob.val.2008@mail.ru>
2026-09-12 18:10:26 +0500

    не доконца доделал генирацию цифр
    проблема в том что побовые бомбы стаят цифры на противо положные стенны,
    так как это все списаок из 81 пункта который делеться на ряды по 9.
    значит самая правая бомба поцентру боля считаеться соседом с самой левой
    на одну ниже ячейкой. надеюсь понятно обьяснил

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use rand::prelude::*;
 
 fn main() {
-    let mut bomb_map = [0; 36];
-    let mut bomb: u8 = 6;
+    let mut bomb_map = [0u8; 81];
+    let mut bomb: u8 = 8;
+
 
     'onecycle: loop {
         for i in 0..bomb_map.len() {
-            if bomb_map[i] == 1 {
+            if bomb_map[i] == 9 {
                 continue;
             } else {
                 if bomb > 0 {
@@ -20,7 +21,7 @@ fn main() {
     // настроил lazygit чтобы не просил каждый раз
 
     fn random(bomb: &mut u8) -> u8 {
-        if rand::rng().random_range(0..4) == 0 {
+        if rand::rng().random_range(0..20) == 0 {
             *bomb -= 1;
             9
         } else {
@@ -28,20 +29,39 @@ fn main() {
         }
     }
 
-    println!("{:?}", bomb_map);
+    // println!("{:?}", bomb_map);
 
     for i in 0..bomb_map.len() {
-        if bomb_map[i] == 9 {
-            if i == 0 {
-                continue;
-            } else {
-                bomb_map[i-1] = 1;
+        let j = i as i16;
+        match bomb_map[i] {
+            0 => (),
+            9 => { numbers(j, &mut bomb_map); },
+            _ => (),
+        };
+    }
+
+    fn numbers(place: i16, map: &mut [u8; 81]) {
+        for i in ((place - 10)..=(place - 8)).chain((place - 1)..=(place + 1)).chain((place + 8)..=(place + 10)) {
+            if i >= 0 && i <= 80 && map[i as usize] != 9{
+                map[i as usize] += 1;
             }
         }
-        // match bomb_map[i] {
-        //     9 => println!("dfsdf")
-        // }
     }
+
+    fn ap_bomb(palce: u8, map: &mut [u8; 81]) {
+        for i in 1..=3 {
+            let new_palce = (palce - (11 - i)) as usize;
+            if map[new_palce] == 9 {
+                continue;
+            } else {
+                map[new_palce] = 1;
+            }
+
+        }
+    }
+
+    // println!("{:?}", bomb_map);
+
 
     #[derive(Debug)]
     enum Inside {
@@ -69,27 +89,17 @@ fn main() {
 
     // println!("{:#?}", cell_map);
 
-    // for i in 0..cell_map.len() {
-    //     match cell_map[i].inside {
-    //         Inside::Bomb => miner(&i),
-    //         Inside::Nule => (),
-    //         _ => println!("тебя пока нет"),
-    //     }
-    // }
-    //
-    // fn miner(i: &usize) {
-    //     println!("{}", i)
-    // }
+    let rows = 9;
+    let cols = 9;
 
-    let rows = 6;
-    let cols = 6;
+    let mut map = vec![vec![ 0 ; cols]; rows];
 
-    let mut map = vec![vec![' '; cols]; rows];
+    let mut check = 0;
 
-    
     for i in 0..rows {
         for j in 0..cols{
-            map[i][j] = '#';
+            map[i][j] = bomb_map[check];
+            check += 1;
         }
     }
 
